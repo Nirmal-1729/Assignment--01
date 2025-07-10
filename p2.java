25abe9f03be5508ed773657d38b7cae818fec452
Nirmal-1729 <nir.k1729@gmail.com>
2025-07-10 23:34:22 +0530

    Add files via upload

---
+++ p2.java
@@ -0,0 +1,145 @@
+class Ap{
+    public static void main(String args []){
+
+        // Q45
+        // int a=-21;
+        // int a2=-18;
+        // int d= -12-(-18);
+        // int n =28;
+        // int s ;
+        // // s=n/2*(2a+(n-1)d)
+        // s=(28/2)*((2*a)+((n-1)*d));
+        // System.out.println(s);
+
+
+        // Q44
+        // int a=-21;
+        // int a2=-18;
+        // int d= -12-(-18);
+        // int n =28;
+        // int l = (a+(n-1*d));
+        // System.out.println(l);
+
+
+        // //Q1
+        // int a=96;
+        // int w=8;
+        // int l= (a/w);
+        // int p= 2*(l+w);
+        // System.out.println(l);
+        // System.out.println(p);
+
+
+        // Q3
+        // int l1=13;
+        // int w1= 7;
+        // int l2=520;
+        // int w2= 140;
+        // int a1= (13*7);
+        // int a2= (520*140);
+        // int t = (a2/a1);
+
+        // System.out.println(t);
+
+        // Q4
+        // int l,w,r,a,u,t;
+        // r=6;
+        // a=(300*150);
+        // u=(a/100);
+        // t=u*r;
+        // System.out.println(t);
+
+        // Q5 
+        // double l,tc,r,a,w,p;
+        // l=20.0;
+        // tc=1600.0;
+        // r=25.0;
+        // a=(tc/r);
+        // w=(a/l);
+        // p=(2*(w+l));
+        // System.out.print(p);
+
+        // Q6
+        // int a,b,c,p,s,pa,area,
+        // a=10;
+        // b=9;
+        // p=36;
+        // c=(p-(a+b));
+        // s=p/2;
+        // pa=(s*(s-a)*(s-b)*(s-c));
+        // area=Math.sqrt(pa);
+
+        // System.out.println(area)
+
+        // Q7
+        // int a,b,h;
+        // a=500;
+        // b=50;
+        // h=(2*a)/b;
+        // System.out.println(h);
+
+        // Q8
+        // double a,h1,h,b;
+        //  a=0.8;
+        //  h1=20.0;
+        //  h=(h1/100);
+        //  b=2*a/h;
+
+        // System.out.println(b);
+
+        // Q9
+        // double l,z,a;
+        // l=20.0;
+        // z=1.37;
+        // a=(1.37/4)*l*l;
+
+        // System.out.println(a);
+        
+        // Q10
+        // double s,a;
+        // s=15.0;
+        // a=0.5*s*s;
+
+        // System.out.println(a);
+
+
+        // Q11
+        // int b=8, h=5, a=320;
+        // int c=(320*2)*40;
+        // int x=Math.sqrt (c);
+
+        // System.out.println();
+
+       // Q12
+        // int a=-21;
+        // int a2=-18;
+        // int d= -12-(-18);
+        // int n =28;
+        // int s ;
+        // s=(28/2)*((2*a)+((n-1)*d));
+
+        // System.out.println(s);
+
+        // //Q13
+        // double h,os,b,a;
+        // h=13;
+        // os=12;
+        // b=(h*h)-(os*os);
+        // b=Math.sqrt(b);
+        // a=(12/2)*5;
+
+        // System.out.println(a);  
+        
+        // Q14
+        int a,l;
+        a=184;
+        l=16;
+        l=(184*2/16);
+        System.out.println(l);
+
+
+
+
+
+    }
+}
